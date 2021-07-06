08956b62ab537c98d49696125e945dcda7d6c39e
ArunavaChowdhury <arunav.pavel@gmail.com>
2021-07-06 04:58:01 -0400

    update

--- src/Components/ContactSection.js
+++ src/Components/ContactSection.js
@@ -29,19 +29,19 @@ const ContactSection = () => {
       <Heading>Contact Me</Heading>
       <StyledForm>
         <InputDiv>
-          <Label name="name">Name</Label>
+          <Label name="name">Name:</Label>
           <Input type="text" name="name" required="true"></Input>
         </InputDiv>
         <InputDiv>
-          <Label email="email">Email</Label>
+          <Label email="email">Email:</Label>
           <Input type="email" name="email" required="true"></Input>
         </InputDiv>
         <InputDiv>
-          <Label subject="subject">Subject</Label>
+          <Label subject="subject">Subject:</Label>
           <Input type="text" name="subject" required="true"></Input>
         </InputDiv>
         <InputDiv>
-          <Label message="message">Message</Label>
+          <Label message="message">Your Message:</Label>
           <StyledTextArea
             type="text"
             name="message"
